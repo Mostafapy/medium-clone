81efa0c6441e1f61562591fdf297f17631c50c8f
Mostafa Hisham <mmham3919@gmail.com>
2023-03-27 11:59:49 +0200

    fix some changes in user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const { asyncHandler } = require('../middlewares/async.middleware');
 const userService = require('../services/user.service');
 
-const add = asyncHandler(async (req, res, next) => {
+const add = async (req, res, next) => {
   try {
     const { body } = req;
     const user = await userService.save(body);
@@ -10,9 +10,9 @@ const add = asyncHandler(async (req, res, next) => {
     res.send(err);
   }
   next();
-});
+};
 
-const getById = asyncHandler(async (req, res, next) => {
+const getById = async (req, res, next) => {
   try {
     const user = await userService.findOneById(req.params.id);
     res.send(user);
@@ -20,25 +20,20 @@ const getById = asyncHandler(async (req, res, next) => {
     res.send(err);
   }
   next();
-});
+};
 
 const follow = asyncHandler(async (req, res, next) => {
   const { id, userId } = req.body;
 
   userService
     .follow(id, userId)
-    .then(() => res.status(200).json({ msg: 'followed' }))
-    .catch(next);
+    .then(() => res.status(200).json({ msg: 'followed' }));
 });
 
 const getProfile = asyncHandler(async (req, res, next) => {
-  try {
-    const { userId } = req.body;
-    const response = await userService.getProfile(req.params.id, userId);
-    res.status(200).json(response);
-  } catch (err) {
-    console.log(err);
-  }
+  const { userId } = req.body;
+  const response = await userService.getProfile(req.params.id, userId);
+  res.status(200).json(response);
 });
 
 return {
