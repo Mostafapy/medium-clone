3cb273353a91648e46c776f429fc137e3c3010fe
Mostafa Hisham <mmham3919@gmail.com>
2023-03-27 03:36:52 +0200

    Add user service

---
+++ server/services/user.service.js
@@ -0,0 +1,54 @@
+const articleModel = require('../models/article.model');
+const userModel = require('../models/user.model');
+
+const create = async (newUserData) => {
+  try {
+    const newUser = await new userModel(newUserData).save();
+    return Promise.resolve(newUser? newUser : 400);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const findOneById = async (id) => {
+  try {
+    const user = await userModel.findOneById(id);
+    return Promise.resolve(user? user: 404);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const followUser = async (id, userId) => {
+  try {
+    const user = await userModel.findOneById(id);
+    await user.follow(userId);
+    return Promise.resolve();
+  } catch (err) {
+    console.error(err);
+    return Promise.reject();
+  }
+};
+
+const getUserProfile = async (id) => {
+  try {
+    const user = await userModel.findOneById(id);
+    const users = await userModel.find({ following: id });
+    if (users.length > 0) {
+      users.forEach(usr => user.addFollower(usr));
+    }
+    const articles = await articleModel.find({ author: id });
+
+    return Promise.resolve({ user, articles });
+  } catch (err) {
+    console.error(err);
+    return Promise.reject();
+  }
+};
+
+module.exports = {
+  create,
+  findOneById,
+  followUser,
+  getUserProfile
+};
